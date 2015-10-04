ff41b69c948439d3400a77762e8d38e2dcae2088
Serkan Yersen <serkanyersen@gmail.com>
2015-10-04 01:01:33 -0700

    bugfix

--- pjson.js
+++ pjson.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var root = __dirname.replace('node_modules/pjson/', '');
-var contents = fs.readFileSync(root + 'package.json');
+var contents = fs.readFileSync(root + '/package.json');
 var pjson = {};
 
 if (contents) {
